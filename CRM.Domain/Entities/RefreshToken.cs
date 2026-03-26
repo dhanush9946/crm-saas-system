9c1ac9c3e6dd63805ba9cd8c8b4bfd9e6bc4e98d
dhanush9946 <dhanushpoothanganam5@gmail.com>
2026-03-26 23:32:10 +0530

    entity created

---
+++ CRM.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace CRM.Domain.Entities
+{
+    public class RefreshToken
+    {
+        public Guid RefreshTokenId { get; private set; }
+
+        public Guid TenantId { get; private set; }
+        public Guid UserId { get; private set; }
+
+        public byte[] TokenHash { get; private set; } = null!;
+
+        public Guid TokenFamilyId { get; private set; }
+
+        public DateTime IssuedAtUtc { get; private set; }
+        public DateTime ExpiresAtUtc { get; private set; }
+
+        public DateTime? RevokedAtUtc { get; private set; }
+
+        public string DeviceId { get; private set; } = null!;
+
+        private RefreshToken() { }
+
+        public RefreshToken(Guid tenantId, Guid userId, byte[] tokenHash)
+        {
+            RefreshTokenId = Guid.NewGuid();
+            TenantId = tenantId;
+            UserId = userId;
+            TokenHash = tokenHash;
+
+            TokenFamilyId = Guid.NewGuid();
+            IssuedAtUtc = DateTime.UtcNow;
+            ExpiresAtUtc = DateTime.UtcNow.AddDays(7);
+        }
+
+        public void Revoke()
+        {
+            RevokedAtUtc = DateTime.UtcNow;
+        }
+    }
+}
---
+++ CRM.Domain/Entities/UserRole.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Domain.Entities
+{
+    public class UserRole
+    {
+        public Guid UserRoleId { get; private set; }
+
+        public Guid TenantId { get; private set; }
+        public Guid UserId { get; private set; }
+        public Guid RoleId { get; private set; }
+
+        public DateTime CreatedAtUtc { get; private set; }
+
+        private UserRole() { }
+
+        public UserRole(Guid tenantId, Guid userId, Guid roleId)
+        {
+            UserRoleId = Guid.NewGuid();
+            TenantId = tenantId;
+            UserId = userId;
+            RoleId = roleId;
+
+            CreatedAtUtc = DateTime.UtcNow;
+        }
+    }
+}
