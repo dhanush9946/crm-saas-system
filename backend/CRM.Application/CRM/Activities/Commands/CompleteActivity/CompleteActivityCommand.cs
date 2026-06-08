c3bfc6c8d6eaed0c53710d3601b56de27ba1ddf6
dhanush9946 <dhanushpoothanganam5@gmail.com>
2026-06-08 22:02:06 +0530

    feat:Activity Complete endpoint created

---
+++ backend/CRM.Application/CRM/Activities/Commands/CompleteActivity/CompleteActivityCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace CRM.Application.CRM.Activities.Commands.CompleteActivity;
+
+public sealed class CompleteActivityCommand : IRequest
+{
+    public Guid ActivityId { get; init; }
+
+    public string RowVersion { get; init; } = string.Empty;
+}
